25f2f12e5c2cee22056e496d30b1820b56fb3e41
TuxThePenguin0 <33564505+TuxThePenguin0@users.noreply.github.com>
2020-02-15 10:07:54 +1100

    Add timekeeper
    Time should persist between reboots now

--- klte.mk
+++ klte.mk
@@ -152,7 +152,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
     $(LOCAL_PATH)/ubuntu/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
-    $(LOCAL_PATH)/ubuntu/ofono.override:system/halium/etc/init/ofono.override
+    $(LOCAL_PATH)/ubuntu/ofono.override:system/halium/etc/init/ofono.override \
+    $(LOCAL_PATH)/ubuntu/timekeeper.conf:system/halium/etc/init/timekeeper.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.t-o.quirk.forcesink=sink.primary_output \
